f1460885245614325e2e3bf5bb78918718565bfc
JABtv <tom.andres31000@gmail.com>
2025-02-20 00:47:24 +0100

    Correction de low bug

--- src/script/js/calculatrice.js
+++ src/script/js/calculatrice.js
@@ -1,25 +1,5 @@
-let display = document.getElementById("display");
+const bouton = document.getElementById("test");
 
-const boutons = document.querySelectorAll("button");
-
-function c() {
-  boutons.forEach((bouton) => {
-    bouton.addEventListener("click", () => {
-      const valeur = bouton.value;
-
-      if (valeur === "C") {
-        display.value = "";
-      } else if (valeur === "=") {
-        // Calculer le résultat
-        try {
-          ecran.value = eval(ecran.value);
-        } catch (error) {
-          ecran.value = "Erreur";
-        }
-      } else {
-        // Ajouter la valeur du bouton à l'écran
-        ecran.value += valeur;
-      }
-    });
-  });
-}
+bouton.addEventListener("click", function () {
+  alert("Vous avez cliqué sur le bouton !");
+});
